914b558cc61fabb9a6a0ed7829ca7bdf9c090f1e
achensen <Erinrjacobsen@gmail.com>
2024-11-11 17:45:02 -0500

    initial commit

---
+++ assets/js/blog.js
@@ -0,0 +1,19 @@
+// TODO: Create a variable that selects the main element, and a variable that selects the back button element
+const mainEl= document.querySelector('main');
+
+const backButton= document.getElementById('back');
+
+backButton.addEventListener('click',() => {
+    window.history.back();
+});
+
+// TODO: Create a function that builds an element and appends it to the DOM
+// function createAndAppendElement(){
+//     const 
+// }
+
+// TODO: Create a function that handles the case where there are no blog posts to display
+
+// TODO: Create a function called `renderBlogList` that renders the list of blog posts if they exist. If not, call the no posts function.
+
+// TODO: Call the `renderBlogList` function
