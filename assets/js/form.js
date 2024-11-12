4e56b0c17f34694dde6e25bfa5b2975567fdc4ce
achensen <Erinrjacobsen@gmail.com>
2024-11-11 19:25:06 -0500

    localstorage

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,7 +4,7 @@ const formEl= document.querySelector('form');
 function inputCheck(event){
     event.preventDefault()
     console.log('input check');
-
+//  If the form is submitted with missing data, display an error message to the user.
     const username= document.querySelector('#username').value.trim();
     const title= document.querySelector('#title').value.trim();
     const content= document.querySelector('#content').value.trim();
@@ -26,8 +26,11 @@ function inputCheck(event){
         
         };
 
-        localStorage.setItem('formInfo', JSON.stringify(formInfo));
-        console.log('info saved');
+        function storeLocalStorage(formInfo) {
+            localStorage.setItem('formInfo', JSON.stringify(formInfo));
+            console.log('info saved');
+
+}
 
         window.location.href = 'blog.html'
 
@@ -38,6 +41,6 @@ function inputCheck(event){
 
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, 
 // then redirect to the blog page using the `redirectPage` function.
-//  If the form is submitted with missing data, display an error message to the user.
+
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
 formEl.addEventListener('submit', inputCheck)
